88fccc32315a30656e233da62cba128cc278524e
HikmatFadrialR <hikmatfadrial1012@gmail.com>
2024-04-06 01:30:12 +0700

    Tipe Data

---
+++ bin/tipe_data.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Membuat variable dengan tipe data
+  String name = "Fad";
+  int umur = 23;
+  double tinggi = 165.7;
+  bool isProgrammer = true;
+
+  // Variable dengan kata kunci var
+  var alamat = "Bandung Barat";
+
+  // Mencetak variable
+  print("Nama saya $name. \nUmur $umur tahun. \nTinggi $tinggi cm.");
+
+  print("Programmer : $isProgrammer");
+  print("Alamat: $alamat");
+}
